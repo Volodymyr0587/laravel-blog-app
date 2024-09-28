2ddfe209657bfed767ca7a58d61b395637f17b79
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-09-28 10:35:29 +0300

    status flash message component

--- resources/views/blog_posts/blog.blade.php
+++ resources/views/blog_posts/blog.blade.php
@@ -4,12 +4,10 @@
 <!-- main -->
 <main class="container">
     <h2 class="header-title">{{ request()->has('search') ? "Seaarch Results For: $search" : 'All Blog Posts' }}</h2>
-    @if (session()->has('status'))
-        <p x-data="{show:true}" x-init="setTimeout(() => show = false, 4000)" x-show="show"
-            style="color: #fff; width:100%;font-size:18px;font-weight:600;text-align:center;background:#5cb85c;padding:17px 0;margin-bottom:6px;">
-            {{ session('status') }}
-        </p>
-    @endif
+
+    <!-- Status message-->
+    <x-flash-messages.status-message />
+
     <div class="searchbar">
         <form action="{{ route('blog.search') }}">
             @csrf
--- resources/views/categories/index-categories.blade.php
+++ resources/views/categories/index-categories.blade.php
@@ -4,12 +4,7 @@
     <div class="categories-list">
         <h1>Categories List</h1>
         <!-- Status message-->
-        @if (session()->has('status'))
-        <p x-data="{show:true}" x-init="setTimeout(() => show = false, 4000)" x-show="show"
-            style="color: #fff; width:100%;font-size:18px;font-weight:600;text-align:center;background:#5cb85c;padding:17px 0;margin-bottom:6px;">
-            {{ session('status') }}
-        </p>
-        @endif
+        <x-flash-messages.status-message />
         {{-- @include('includes.flash-message') --}}
         @foreach ($categories as $category)
             <div class="item">
