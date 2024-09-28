81a76491ab52ade3aaa9a5bdcf0bfb7aa72c55d6
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-09-28 08:41:11 +0300

    create category page, migration, controller

---
+++ resources/views/categories/create-category.blade.php
@@ -0,0 +1,40 @@
+@extends('layout')
+
+@section('head')
+{{-- CKEditor style --}}
+<style>
+    .ck-editor__editable_inline {
+        min-height: 300px;
+    }
+</style>
+@endsection
+
+@section('main')
+<main class="container" style="background-color: #fff;">
+    <section id="contact-us">
+        <h1 style="padding-top: 50px;">Create New Category!</h1>
+        <!-- Status message-->
+        @if (session()->has('status'))
+        <p x-data="{show:true}" x-init="setTimeout(() => show = false, 4000)" x-show="show"
+            style="color: #fff; width:100%;font-size:18px;font-weight:600;text-align:center;background:#5cb85c;padding:17px 0;margin-bottom:6px;">
+            {{ session('status') }}
+        </p>
+        @endif
+        <!-- Contact Form -->
+        <div class="contact-form">
+            <form action="{{ route('categories.store') }}" method="POST" >
+                @csrf
+                <!-- Name -->
+                <label for="name"><span>Name</span></label>
+                <input type="text" id="name" name="name" value="{{ old('name') }}" />
+                @error('name')
+                    <p style="color: red; margin-bottom: 25px;">{{ $message }}</p>
+                @enderror
+                <!-- Button -->
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+
+    </section>
+</main>
+@endsection
